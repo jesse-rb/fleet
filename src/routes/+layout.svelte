95a2776c707702c615f30553354c0cfc699dc664
jesse-rb <jesse.reynekebarnard@gmail.com>
2023-07-03 22:22:20 +1200

    polish navbar a bit more

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,9 @@
 </script>
 
 <Navbar pages={ [{path: '/', title: 'Home'}, {path: '/run', title: 'Run'}] } activePathName={ $page.url.pathname } ></Navbar>
-    
-<h1>{ $page.data.title ?? 'Unkown Page' }</h1>
-<slot></slot>
+
+<div class="pl-8 pr-8 pt-4 pb-4">
+    <h1>{ $page.data.title ?? 'Unkown Page' }</h1>
+    <slot></slot>
+</div>
 
--- src/routes/navbar/navbar.svelte
+++ src/routes/navbar/navbar.svelte
@@ -5,10 +5,8 @@
     export let activePathName: string;
 </script>
 
-<div class="flex bg-blue-200">
-    { #each pages as {path, title} }
-        <div class="p-1 { activePathName == path ? 'bg-blue-100' : '' }">
-            <a href={ path }>{ title }</a>
-        </div>
+<div class="flex bg-gray-600 pl-8 pr-8">
+    { #each pages as {path, title}, index }
+        <a class="{ activePathName === path ? 'bg-gray-700' : 'bg-gray-600' } pt-2 pb-2 pl-6 pr-6 border-gray-800 border-r-4" class:border-l-4={ index === 0 } href={ path }>{ title }</a>
     {/each}
 </div>
