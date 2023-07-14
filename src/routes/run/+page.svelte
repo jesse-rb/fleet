31ca570459afbb422193a23f8d54071995b23d2b
jesse-rb <jesse.reynekebarnard@gmail.com>
2023-07-14 23:10:39 +1200

    we have a threejs cube

--- src/routes/run/+page.svelte
+++ src/routes/run/+page.svelte
@@ -1,27 +1,51 @@
-<script>
+<script lang="ts">
     // @ts-ignore TODO: quick fix
     import * as THREE from 'three';
     import { onMount } from 'svelte';
 
-    let sceneContainer:?HTMLDivElement = null;
+    let sceneContainer:any = null;
 
     onMount(() => {
-        const width = sceneContainer.innerWidth;
-        const height = sceneContainer.innerHeight;
+        const sceneContainerRect = sceneContainer.getBoundingClientRect();
+        const width = sceneContainerRect.width;
+        const height = sceneContainerRect.height;
 
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
         const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild( renderer.domElement );
+        renderer.setSize(width, height);
+        sceneContainer.appendChild( renderer.domElement );
+
+        const geom = new THREE.BoxGeometry(1, 1, 1);
+        const mat = new THREE.MeshBasicMaterial({color: 0x00ff00 });
+        const cube = new THREE.Mesh(geom, mat);
+
+        scene.add(cube);
+        camera.position.z = 5;
+
+        function animate() {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        }
+
+        animate();
     });
+
+    
 
 </script>
 
 <div>
 
-    <div bind:this={ sceneContainer }>
+    <div id="scene-container" bind:this={ sceneContainer }>
 
     </div>
 </div>
+
+<style>
+    #scene-container {
+        width: 1600px;
+        height: 900px;
+    }
+</style>
